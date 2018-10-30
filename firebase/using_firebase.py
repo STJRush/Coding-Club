055cf3492ddd6b7ddb17ed01770b8efefd90cbc5
Daniel Murray <daniel.murray@stjosephsrush.ie>
2018-10-30 22:35:37 +0000

    Tidy to make sense

--- firebase/using_firebase.py
+++ firebase/using_firebase.py
@@ -5,40 +5,43 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://cookietest-a4f79.firebaseio.com', None)
 
-print("Here's the first value from your database")
-
-temperature = 42
+temperature = 42  #these could come in from a sensor but they're fixed just as an example
 humidity = 41
 
 
-#Patches on extra data without making a random number identifier
-result = firebase.patch('/sensor/dht/', {'First_Vals': 5, 'Sec_Vals': 6})
+#READING DATA
 
-#Patches on extra data without making a random number identifier
-result = firebase.patch('/sensor/dht2/', {'First_Vals': 4, 'Sec_Vals': 2})
-
-
-#reads the database
+#reads the database at the node /dht
 result = firebase.get('/dht', None)
 print(result)
 
-
-
-"""
-#reads the database at /dht/humidity to get that value
+#you can also read along the tree further down /dht/humidity 
 result = firebase.get('/dht/humidity', None)
 print(result)
 
-#deletes code from the database
-firebase.delete('/datas', None)
-print("Right, that's deleted. Lets run the read database code again and see if it's gone")
 
-#write to the database (PUT MEANS UPDATE)
+#CHANGING/ADDING/REMOVING DATA
+
+#deletes code from the database
+firebase.delete('/dht', None)
+
+
+#Patches on extra data from our temp and humidity variables at the top of the program, simulated sensor readings
+result = firebase.patch('/sensor/dht/', {'First_Vals': temperature, 'Sec_Vals': humidity})
+
+#Patches on extra data, you can also hard code in numbers like below
+result = firebase.patch('/sensor/dht2/', {'First_Vals': 4, 'Sec_Vals': 2})
+
+#Best to use PATCH instead of PUT or POST as it avoids generating a large random number for the node name
+
+
+#write to the database (PUT means update current values)
 firebase.put("/dht", "/humidity", "0.00")
 firebase.put("/dht", "/humidity", "1.00")
 
-#POSTS  to the database (POST means create)
+
+#POSTS  to the database (POST means create an entry, does generate a nasty long node name)
 data = {"temp": temperature, "humidity": humidity}
 firebase.post('/dht', data)
-"""
+
 
