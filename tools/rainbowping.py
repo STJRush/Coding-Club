0180f7fdc8c5c4f3795eb6a82d2e078f034f0064
Daniel Murray <daniel.murray@stjosephsrush.ie>
2020-09-25 10:04:33 +0100

    updated IPs

--- tools/rainbowping.py
+++ tools/rainbowping.py
@@ -1,7 +1,7 @@
 
 #pings a bunch of switches or whatever to see if the internet is working. The command line was sad so I added some colour.
 
-pingTargets = ["192.168.8.100", "192.168.8.105", "192.168.8.125"]
+pingTargets = ["10.1.199.09","10.1.199.10","10.1.199.11", "10.1.199.12", "10.1.199.13", "10.1.199.14"]
 
 
 from ping3 import ping, verbose_ping
@@ -12,15 +12,16 @@ from colorama import init
 init()
 from colorama import Fore, Back, Style
 
-
+severityLevel = 0 
 
 # Starting the mixer
 mixer.init()
 
 # Loading the songs and sounds
 mixer.music.load("birds.mp3")
-crash_sound = mixer.Sound("crash.mp3")
-malf_sound = mixer.Sound("malf.mp3")
+#crash_sound = mixer.Sound("crash.mp3")
+#soundy = mixer.Sound("malf.wav")
+#mixer.Sound.play(soundy)
 
 # Setting the volume
 mixer.music.set_volume(0.7)
@@ -95,33 +96,57 @@ try:
 
             if ping(ip) == None:
                 print(Fore.RED + Back.BLACK + "WARNING! ENTIRE SWITCH IS DOWN!")
-                mixer.Sound.play(malf_sound)
+                #mixer.Sound.play(malf_sound)
                 print(Style.RESET_ALL)
 
             else:
-                snapshot=ping(ip)*1000
+                
+                try:  #this is because sometimes it wasn't working
+                 snapshot=ping(ip)*1000
+                except:
+                 snapshot = 42 #this is my favorite number
+                 print("Oooops")
 
 
                 if snapshot < 10:
                     print(Fore.BLACK + Back.GREEN + "We're all good")
-                    mixer.music.stop()
-                    mixer.music.load("birds.mp3")
-                    mixer.music.play()
+                    
+                    if severityLevel == 0:
+                    
+                        mixer.music.stop()
+                        mixer.music.load("birds.mp3")
+                        mixer.music.play()
+                    
+                    
                     sleep(1)
+                    
+                    severityLevel = 0
 
                 elif snapshot > 10 and snapshot < 100:
                     print(Fore.BLACK + Back.YELLOW + "Could be better")
-                    mixer.music.stop()
-                    mixer.music.load("drizzle.mp3")
-                    mixer.music.play()
+                    
+                    if severityLevel == 1:
+                    
+                        mixer.music.stop()
+                        mixer.music.load("drizzle.mp3")
+                        mixer.music.play()
+                    
                     sleep(1)
+                    
+                    severityLevel = 1
 
                 elif snapshot > 100 and snapshot < 500:
                     print(Fore.BLACK + Back.RED + "Not great, not terrible")
-                    mixer.music.stop()
-                    mixer.music.load("rain.mp3")
-                    mixer.music.play()
+                   
+                    if severityLevel == 2:
+                    
+                        mixer.music.stop()
+                        mixer.music.load("rain.mp3")
+                        mixer.music.play()
+                    
                     sleep(1)
+                    
+                    severityLevel = 2
 
                 elif snapshot > 500 and snapshot < 1000:
                     print(Fore.WHITE + Back.RED + "PANIC PANIC PANIC")
@@ -129,13 +154,17 @@ try:
                     mixer.music.load("jaziswind.mp3")
                     mixer.music.play()
                     sleep(1)
+                    
+                   
 
 
                 elif snapshot > 1000:
                     print("ALARM ALARM")
                     print(Fore.WHITE + Back.RED + "IT'S HAPPENING AGAIN!")
                     print(Fore.WHITE + Back.RED + "RUN FOR YOUR LIVES!")
-                    mixer.Sound.play(crash_sound)
+                    #mixer.Sound.play(crash_sound)
+                    
+                    
 
 
                 else:
