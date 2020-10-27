289ab7b73b9a77a25f0b9a09cb777afb2673e0ba
Daniel Murray <daniel.murray@stjosephsrush.ie>
2020-10-27 14:09:00 +0000

    Update MusicAndSounds.py
    update sounds

--- pygame_Sounds/MusicAndSounds.py
+++ pygame_Sounds/MusicAndSounds.py
@@ -1,5 +1,12 @@
+#This file will load an .mp3 music clip and a .wav sound in the same folder as itself
+#You will need to download these files to run this .py file.
+
+# You can find the files here:
+# https://github.com/STJRush/Coding-Club/tree/master/pygame_Sounds
+
 # These 3 lines of code must go at the top of your program for sounds or music to work
-# You need to also install the pygame module from "Tools>Manage Packages>" & search pygame
+# You need to also install the pygame module. On the python editor Thonny, you can easily
+# do this from "Tools>Manage Packages>" & search pygame, then click "install".
 
 from pygame import mixer 
 from time import sleep
@@ -62,7 +69,51 @@ sleep(1)
 #stops music
 mixer.music.stop()
 
-#note: Those last few lines won't work without first loading the sound in parts 1/3 and 2/3
+#note: Those last few lines won't work without first loading the sounds on line 44 and 45
+
+
+"""
+Classic mistakes along the way to getting this working:
+
+- "No module called pygame found"
+
+You need to install the pygame module to run this code. On the python editor "Thonny", you can easily
+ do this from "Tools>Manage Packages>" & search pygame, then click "install". If you're not using
+ you should google "how to install pygame in...<whatever you're using> as each program is a bit different.
+
+ 
+"FileNotFoundError: No such file or directory"
+
+a) You've not downloaded the sound into the same folder as your python file
+b) You've a spelling mistake or typo in the name of your file
+c) You've forgotten the file extension eg. picard instead of picard.wav
+
+
+"Cannot play this sound"
+
+You're trying play .wavs (sounds) with the code for music (.mp3) or vis versa.
+mixer.Sound is for sounds and mixer.music is for music. 
+
+
+
+"AttributeError: module 'pygame.mixer' has no attribute 'sound'"
+
+
+It's pygame.Sound with a capital S, not pygame.sound with a lowercase "s".
+
+
+
+"I get no errors but no sounds plays!!!"
+
+- Go to youtube and search Rasputin. It's a cracking song. It'll also test your sound.
+
+- After this, ensure you've a second or two of pause sleep(2) to allow the sound time to play.
+
+
+"""
+
+
+
 
 
 
