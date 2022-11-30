45488b9c4c991f6139401c9fb74c3f9c7bc44448
Daniel Murray <daniel.murray@stjosephsrush.ie>
2022-11-30 01:12:02 +0000

    Update main.py

--- emotional_mouse/main.py
+++ emotional_mouse/main.py
@@ -1,40 +1,81 @@
-# emotional mouse code
+# An emotional mouse code
 from machine import Pin
 import time
 
-led = Pin("LED", machine.Pin.OUT)
-led.off()
-time.sleep(1)
-c=0
-pinIn = Pin(15, Pin.IN,Pin.PULL_UP) # sensor pin
-pinOut = Pin(16, Pin.OUT) # output pin
+# sets up internal LED on pico
+interalLED = Pin("LED", machine.Pin.OUT)
+
+# Setup of GPIO Pins
+pinIn15 = Pin(15, Pin.IN,Pin.PULL_UP) # sensor pin
+soundPin16 = Pin(16, Pin.OUT) # output pin for relay control of Sound Board
+mouseLEDPin17 = Pin(17, Pin.OUT) # output pin led that controls mouse
+
+# initialize counter for later
+i=0
 
 print("Starting..")
 
+# Turn on mouse motion tracking LED (The one that's normally on the mouse)
+mouseLEDPin17.on()
 
 while True:
     
-    if pinIn.value(): # if we see an input signal on Pin15
-        led.on()
+    # if we see an input signal on Pin15...
+    if pinIn15.value(): 
+        # interalLED.on() # turn on the interal LED (debugging)
         print("We get signal")
-        pinOut.value(1) # turns on output Pin16
-        time.sleep(1)
-    else:
-        led.off()
-        pinOut.value(0) # turns off output Pin16
+        soundPin16.on() # turns on output Pin16 to trigger relay-> soundboard
         
-        # report nothing yet every 10 cycles to confirm program still running
-        c=c+1
-        if c%10 == 1:
+        # turn off/on the mouse for a bit to punish user for being too rough
+        mouseLEDPin17.off()
+        time.sleep(0.5)
+        mouseLEDPin17.on()
+        time.sleep(1)
+        mouseLEDPin17.off()
+        time.sleep(1)
+        mouseLEDPin17.on()
+        
+    else:
+        # interalLED.off()  (debugging)
+        soundPin16.off() # turns off output Pin16 to stop playing sound
+        
+        # print "nothing yet" every 10 cycles to confirm program still running
+        i=i+1
+        if i%10 == 1:
             print("nothing yet")
-            
+    
+    #tick interval
     time.sleep(0.1)
     
-print("done")
-
-
-for x in range(4):
-    led.on()
-    time.sleep(0.1)
-    led.off()
-    time.sleep(0.1)
+    
+"""
+Raspberry Pi Pico
+   _______
+ __[     ]__
+[  [MICRO]  ]
+[  [ USB ]  ]
+[  [_____]  ]
+[           ]
+[ 0    VBUS ]            Fantastic GPIO Pins and where to find them
+[ 1    VSYS ] 
+[ GND  GND  ]       <----   GND is 3rd pin down from top right near USB
+[ 2    3V3  ] (EN) 
+[ 3    3v3  ] (OUT) <----   +3V is 5th pin down from top right near USB
+[ 4    REF  ] (ADC_VREF)
+[ 5    28   ] (ADC2) <----  Analogue Input is 7th pin down from top right near USB
+[ GND  GND  ] (AGND)
+[ 6    27   ] (ADC1)
+[ 7    26   ] (ADC0)
+[ 8    RUN  ]
+[ 9    22   ]
+[ GND  GND  ]
+[ 10   21   ]
+[ 11   20   ]
+[ 12   19   ]
+[ 13   18   ]
+[ GND  GND  ] <----   GND is 3rd pin up on the right from the bottom of the Pico, either side.
+[ 14   17   ] <----   GPIO17 is 2nd last pin down from top right near USB (IN THIS PROJECT: MOUSE MOTION RED LED)
+[ 15   16   ] <----   GPIO16 is last pin down from top right near USB (IN THIS PROJECT: RELAY/SOUNDBOARD)
+[   o o o   ]
+ -----------
+"""
